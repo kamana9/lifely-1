f84f65fcd1d5862c152331a6891d0df00a608b05
Rohan <rohanshakya254@gmail.com>
2021-06-28 13:55:04 +0545

    added feedbacks model and integrate with UI

--- lifely/visitor_side/admin.py
+++ lifely/visitor_side/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Aboutus
+from .models import Aboutus, FeedBacks
+
 
 admin.site.register(Aboutus)
+
+
+class FeedBacksAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
+    list_filter = ('email',)
+
+
+admin.site.register(FeedBacks, FeedBacksAdmin)
--- lifely/visitor_side/views.py
+++ lifely/visitor_side/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .forms import FeedbackForm
 # from django.http import HttpResponse
 # from django .views .generic .list import ListView
 # from .models import Aboutus
 
 
 def index(request):
-    return render(request, "visitor_side/index.html")
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = FeedbackForm
+    return render(request, "visitor_side/index.html", {
+        'form': form
+    })
 
 
 def about_us(request):
